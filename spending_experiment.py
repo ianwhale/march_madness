4bbbe408f065e6de2cabd696e27672436a9d2498
ianwhale <ianwhale@github.com>
2018-04-14 09:54:13 -0400

    Update spening experiment.
    - Now uses top 25.
    - Add option to remove outliers.
    - Update previous plot, add new plot with outliers removed.

--- spending_experiment.py
+++ spending_experiment.py
@@ -7,14 +7,15 @@ import pandas as pd
 import numpy as np
 from utils import append_glicko
 import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
 from scipy.stats import pearsonr
+from matplotlib.lines import Line2D
+
 
 def main():
     """
     Main entry point.
     """
-    spending2017 = {  # NCAAB spending for 2016-17 season by top-ten Glicko-2 team ID.
+    spending2017 = {     # NCAAB spending for 2016-17 season by top-ten Glicko-2 team ID.
         1242: 11126047,  # Kansas
         1437: 11120378,  # Villanova
         1246: 19180059,  # Kentucky
@@ -24,31 +25,70 @@ def main():
         1181: 19507686,  # Duke
         1332: 9229004,   # Oregon
         1257: 17065364,  # Louisville
-        1438: 8555125    # Virgina
+        1438: 8555125,   # Virgina
+        1452: 9453611,   # West Virginia
+        1124: 9097264,   # Baylor
+        1374: 7269481,   # Southern Methodist
+        1345: 8099562,   # Purdue
+        1323: 6793853,   # Notre Dame
+        1235: 6858775,   # Iowa State
+        1458: 9564602,   # Wisconsin
+        1455: 6380482,   # Wichita State
+        1417: 9856861,   # UCLA
+        1274: 7547589,   # Miami FL
+        1268: 7442558,   # Maryland
+        1153: 6970262,   # Cincinnati
+        1139: 5489704,   # Butler
+        1276: 16913194,  # Michigan
+        1277: 10975215,  # Michigan State
     }
+
+    N = 25
+    remove_outliers = True
+    outlier_spending = 15000000  # Teams that spend more than $15,000,000 on basketball are considered outliers.
+
+    assert N == len(spending2017)
 
     df, glicko_dict = append_glicko(pd.read_csv('./data/RegularSeasonCompactResults.csv'))
     teams = pd.read_csv('./data/Teams.csv')
-    top_ten = sorted(glicko_dict.items(), key=lambda x: x[1].getRating(), reverse=True)[:10]
-    top_ten_ids, top_ten_glicko = [i[0] for i in top_ten], [i[1].getRating() for i in top_ten]
-    ordered_spending = [spending2017[tid] for tid in top_ten_ids]
+    top_N = sorted(glicko_dict.items(), key=lambda x: x[1].getRating(), reverse=True)[:N]
+    top_N_ids, top_N_glicko = [i[0] for i in top_N], [i[1].getRating() for i in top_N]
+    ordered_spending = [spending2017[tid] for tid in top_N_ids]
 
-    print("Corrcoef for spending and glicko: {}".format(pearsonr(ordered_spending, top_ten_glicko)))
+    if remove_outliers:
+        inlier_spending, inlier_glicko, inlier_ids = [], [], []
+        for tid, glicko, spending in zip(top_N_ids, top_N_glicko, ordered_spending):
+            if spending < outlier_spending:
+                inlier_spending.append(spending)
+                inlier_glicko.append(glicko)
+                inlier_ids.append(tid)
+
+        top_N_ids = inlier_ids
+        top_N_glicko = inlier_glicko
+        ordered_spending = inlier_spending
+
+    r_val = pearsonr(ordered_spending, top_N_glicko)
+    print("Corrcoef for spending and glicko: {}".format(r_val))
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    for tid, glicko, spending in zip(top_ten_ids, top_ten_glicko, ordered_spending):
+    for tid, glicko, spending in zip(top_N_ids, top_N_glicko, ordered_spending):
         ax.scatter(spending, glicko, c='b')
-        ax.annotate(str(teams.loc[teams.TeamID == tid].TeamName.values[0]),
-                    xy=(spending, glicko),
-                    xytext=(spending + 1e-5 * spending, glicko + 1e-3 * glicko))
+        # ax.annotate(str(teams.loc[teams.TeamID == tid].TeamName.values[0]),
+        #            xy=(spending, glicko),
+        #             xytext=(spending + 1e-5 * spending, glicko + 1e-3 * glicko))
 
-    z = np.polyfit(ordered_spending, top_ten_glicko, 1)
+    z = np.polyfit(ordered_spending, top_N_glicko, 1)
     p = np.poly1d(z)
     plt.plot(ordered_spending, p(ordered_spending))
     plt.ylabel("Glicko-2 Rating")
-    plt.xlabel("Men's 2017 Basketball Expenses")
-    plt.savefig("./plots/glickos_by_spending.png", dpi=300)
+    plt.xlabel("Men's 2016-17 Basketball Expenses")
+    plt.legend(
+        [Line2D([0], [0], color='b', lw=3)], ["r = {}".format(round(r_val[0], 4))]
+    )
+    f_name = "glickos_by_spending.png" if not remove_outliers else "glickos_by_spending_no_outliers.png"
+    f_name = "./plots/" + f_name
+    plt.savefig(f_name, dpi=300)
     plt.show()
 
 
