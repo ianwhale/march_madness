591e71ec90a2262d9f2c9bd98f1e5ab882d0b18b
ianwhale <ianwhale@github.com>
2018-04-27 16:31:10 -0400

    Update correlation visualization.

--- cov_plot.py
+++ cov_plot.py
@@ -7,21 +7,43 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from utils import remove_init_rows
+from utils import remove_init_rows, drop_irrelevant_columns
 
 
 def main():
     """
     Main entry point.
     """
-    filename = "data_matrices/2017season.csv"
+
+    from pprint import PrettyPrinter
+    print = PrettyPrinter(indent=4).pprint
+
+    filename = "data_matrices/DataMatrices/1_seasons/1_seasons_combined.csv"
     df = pd.read_csv(filename)
     df = remove_init_rows(df)
-
-    df = df.drop(['Unnamed: 0', 'year', 'id_0', 'id_1'], axis=1)
+    df = drop_irrelevant_columns(df)
     df = df.drop(['label'], axis=1)
 
-    cov = df.corr()
+    #
+    # Add columns corresponding to the same feature.
+    # This will not affect correlation of pairwise features, e.g. rpi-0 and rpi-1 are the same feature.
+    #
+
+    headers = df.columns.values
+
+    zeros = headers[:len(headers) // 2]
+    ones = headers[len(headers) // 2:]
+
+    headers = []
+    for name in zeros:
+        headers.append(name.replace("opponents", "ops")[:-2])
+
+    zeros = df[zeros]
+    ones = df[ones]
+
+    summed = pd.DataFrame(data=zeros.as_matrix() + ones.as_matrix(), columns=headers)
+
+    cov = summed.corr()
 
     mask = np.zeros_like(cov, dtype=np.bool)
     mask[np.triu_indices_from(mask)] = True
