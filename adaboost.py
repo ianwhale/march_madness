af0fe8b07a4b3704bce18b46b45eab83d06a471e
ianwhale <ianwhale@github.com>
2018-04-27 16:29:33 -0400

    Update Adaboost experiment.

--- adaboost.py
+++ adaboost.py
@@ -2,25 +2,30 @@
 # adaboost.py
 #   - Classify data into wins/losses with AdaBoost algorithm.
 #
-# 2017 Data Results
-# Mean validation accuracy: 0.673804012345679
-# Testing accuracy: 0.6567164179104478
-# Best estimators found: 75
-#
-# 2014 - 2017 Data Results
-# Mean validation accuracy: 0.6804448563484709
-# Testing accuracy: 0.6716417910447762
-# Best estimators found: 225
-#
-# 4 Seasons Combined
-# Mean validation accuracy: 0.6912350597609562
+# data_matrices/DataMatrices/old_glicko_1_seasons/old_glicko_1_seasons_combined.csv
+# Mean validation accuracy: 0.7052604365100963
+# Testing accuracy: 0.7054026503567788
+# Best estimators found: 90
+
+# data_matrices/DataMatrices/1_seasons/1_seasons_combined.csv
+# Mean validation accuracy: 0.6967425025853154
+# Testing accuracy: 0.7043832823649337
+# Best estimators found: 120
+
+# data_matrices/DataMatrices/2_seasons/2_seasons_combined.csv
+# Mean validation accuracy: 0.6957675264963676
+# Testing accuracy: 0.707742639040349
+# Best estimators found: 70
+
+# data_matrices/DataMatrices/3_seasons/3_seasons_combined.csv
+# Mean validation accuracy: 0.6902847819638089
+# Testing accuracy: 0.7151230949589683
+# Best estimators found: 110
+
+# data_matrices/DataMatrices/4_seasons/4_seasons_combined.csv
+# Mean validation accuracy: 0.6916334661354582
 # Testing accuracy: 0.7084917617237009
-# Best estimators found: 50
-#
-# Old Glicko 2017
-#
-#
-#
+# Best estimators found: 40
 
 import os
 import pickle
@@ -33,18 +38,19 @@ from sklearn.model_selection import GridSearchCV
 from utils import remove_init_rows, get_data_and_labels, get_tourney_reg_season, drop_irrelevant_columns
 
 
-def main():
+def main(filename):
     """
     Main entry point.
+    :param filename: string, the full filepath.
     """
-    filename = "data_matrices/DataMatrices/4_seasons/4_seasons_combined.csv"
     df = pd.read_csv(filename)
     df = remove_init_rows(df)
     reg_season_df, tourney_df = get_tourney_reg_season(df)
     X, y = get_data_and_labels(drop_irrelevant_columns(reg_season_df))
     X_test, y_test = get_data_and_labels(drop_irrelevant_columns(tourney_df))
 
-    model_file = os.path.join(".", "models", "adaboost.pkl")
+    base_name = os.path.basename(filename).split(".")[0]
+    model_file = os.path.join(".", "models", base_name + "_adaboost.pkl")
     if os.path.isfile(model_file):
         with open(model_file, 'rb') as fptr:
             clf = pickle.load(fptr)
@@ -53,7 +59,7 @@ def main():
         ada = AdaBoostClassifier(DecisionTreeClassifier(max_depth=1))
         clf = GridSearchCV(ada,
                            {
-                                'n_estimators': [25, 35, 50, 65, 75]
+                                'n_estimators': [i for i in range(40, 121, 10)]
                            },
                            n_jobs=-1,
                            cv=5)
@@ -92,9 +98,19 @@ def main():
     plt.xticks(indx, features, rotation="vertical")
     plt.ylabel("Adaboost Feature Importance")
     plt.tight_layout()
-    plt.savefig("./plots/adaboost_feature_importance.png", dpi=300)
-    plt.show()
+    plt.savefig("./plots/" + base_name + "_adaboost_feature_importance.png", dpi=300)
+    plt.close()
 
 
 if __name__ == '__main__':
-    main()
+    filepaths = [
+        "data_matrices/DataMatrices/old_glicko_1_seasons/old_glicko_1_seasons_combined.csv",
+        "data_matrices/DataMatrices/1_seasons/1_seasons_combined.csv",
+        "data_matrices/DataMatrices/2_seasons/2_seasons_combined.csv",
+        "data_matrices/DataMatrices/3_seasons/3_seasons_combined.csv",
+        "data_matrices/DataMatrices/4_seasons/4_seasons_combined.csv"
+    ]
+
+    for f in filepaths:
+        print(f)
+        main(f)
