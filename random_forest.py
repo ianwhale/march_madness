21176ff94a61ca17a931b22bb3dcf16da6553b57
ianwhale <ianwhale@github.com>
2018-04-27 16:31:44 -0400

    Update Random Forest experiment.

--- random_forest.py
+++ random_forest.py
@@ -2,24 +2,30 @@
 # random_forest.py
 #   - Classify data into wins/losses with random forest.
 #
-# 2017 Data Results
-# Mean validation accuracy: 0.6739969135802469
-# Testing accuracy: 0.7014925373134329
-# Best estimators found: 55
-#
-# 2014-2017 Data Results
-# Mean validation accuracy: 0.6721037998146432
-# Testing accuracy: 0.6716417910447762
-# Best estimators found: 70
-#
-# 4 Seasons Combined
-#
-#
-#
-# Old Glicko 2017 Results
-#
-#
-#
+# data_matrices/DataMatrices/old_glicko_1_seasons/old_glicko_1_seasons_combined.csv
+# Mean validation accuracy: 0.7022396995591357
+# Testing accuracy: 0.7043832823649337
+# Best estimators found: 170
+
+# data_matrices/DataMatrices/1_seasons/1_seasons_combined.csv
+# Mean validation accuracy: 0.6939939040984052
+# Testing accuracy: 0.7023445463812437
+# Best estimators found: 160
+
+# data_matrices/DataMatrices/2_seasons/2_seasons_combined.csv
+# Mean validation accuracy: 0.6928921670764401
+# Testing accuracy: 0.7099236641221374
+# Best estimators found: 180
+
+# data_matrices/DataMatrices/3_seasons/3_seasons_combined.csv
+# Mean validation accuracy: 0.6840403441115396
+# Testing accuracy: 0.7080890973036342
+# Best estimators found: 160
+
+# data_matrices/DataMatrices/4_seasons/4_seasons_combined.csv
+# Mean validation accuracy: 0.680605577689243
+# Testing accuracy: 0.6932826362484157
+# Best estimators found: 180
 
 import os
 import pickle
@@ -31,18 +37,19 @@ from sklearn.model_selection import GridSearchCV
 from utils import remove_init_rows, get_data_and_labels, drop_irrelevant_columns, get_tourney_reg_season
 
 
-def main():
+def main(filename):
     """
     Main entry point.
+    :param filename: string, the full filepath.
     """
-    filename = "data_matrices/DataMatrices/4_seasons/4_seasons_combined.csv"
     df = pd.read_csv(filename)
     df = remove_init_rows(df)
     reg_season_df, tourney_df = get_tourney_reg_season(df)
     X, y = get_data_and_labels(drop_irrelevant_columns(reg_season_df))
     X_test, y_test = get_data_and_labels(drop_irrelevant_columns(tourney_df))
 
-    model_file = os.path.join(".", "models", "random_forest.pkl")
+    base_name = os.path.basename(filename).split(".")[0]
+    model_file = os.path.join(".", "models", base_name + "_random_forest.pkl")
     if os.path.isfile(model_file):
         with open(model_file, 'rb') as fptr:
             clf = pickle.load(fptr)
@@ -51,7 +58,7 @@ def main():
         rf = RandomForestClassifier(n_estimators=50)
         clf = GridSearchCV(rf,
                            {
-                                'n_estimators': [i for i in range(40, 101, 5)]
+                                'n_estimators': [i for i in range(90, 201, 10)]
                            },
                            n_jobs=-1,
                            cv=5)
@@ -90,9 +97,19 @@ def main():
     plt.xticks(indx, features, rotation="vertical")
     plt.ylabel("Random Forest Feature Importance")
     plt.tight_layout()
-    plt.savefig("./plots/random_forest_feature_importance.png", dpi=300)
-    plt.show()
+    plt.savefig("./plots/" + base_name + "_random_forest_feature_importance.png", dpi=300)
+    plt.close()
 
 
 if __name__ == '__main__':
-    main()
+    filepaths = [
+        "data_matrices/DataMatrices/old_glicko_1_seasons/old_glicko_1_seasons_combined.csv",
+        "data_matrices/DataMatrices/1_seasons/1_seasons_combined.csv",
+        "data_matrices/DataMatrices/2_seasons/2_seasons_combined.csv",
+        "data_matrices/DataMatrices/3_seasons/3_seasons_combined.csv",
+        "data_matrices/DataMatrices/4_seasons/4_seasons_combined.csv"
+    ]
+
+    for f in filepaths:
+        print(f)
+        main(f)
