b4634c67a29b8d55130b2728ff82e21af2cd9ede
ianwhale <ianwhale@github.com>
2018-05-04 10:49:59 -0400

    Clean up utils code.

--- utils.py
+++ utils.py
@@ -4,25 +4,24 @@
 
 import pandas as pd
 import numpy as np
-from glicko2.glicko2 import Player, glicko_rounds
 
 
-def append_glicko(df):
-    """
-    Appends the glicko2 ratings of the winning and losing team to each row.
-    * Dataframe should probably only be the regular season.
-    :param df: pandas.DataFrame, some MM data with WTeamID and LTeamID columns.
-    :return: (pandas.DataFrame, dict) same dataframe but with w_glicko and l_glicko appended and glicko score dict.
-    """
-    team_ids = set(df.WTeamID).union(set(df.LTeamID))
-    glicko = {team_id: Player() for team_id in team_ids}
-
-    w_glicko, l_glicko = glicko_rounds(glicko, [], df)
-
-    df['w_glicko'] = w_glicko
-    df['l_glicko'] = l_glicko
-
-    return df, glicko
+# def append_glicko(df):
+#     """
+#     Appends the glicko2 ratings of the winning and losing team to each row.
+#     * Dataframe should probably only be the regular season.
+#     :param df: pandas.DataFrame, some MM data with WTeamID and LTeamID columns.
+#     :return: (pandas.DataFrame, dict) same dataframe but with w_glicko and l_glicko appended and glicko score dict.
+#     """
+#     team_ids = set(df.WTeamID).union(set(df.LTeamID))
+#     glicko = {team_id: Player() for team_id in team_ids}
+#
+#     w_glicko, l_glicko = glicko_rounds(glicko, [], df)
+#
+#     df['w_glicko'] = w_glicko
+#     df['l_glicko'] = l_glicko
+#
+#     return df, glicko
 
 
 def get_labels(df):
@@ -70,15 +69,6 @@ def get_data_and_labels(df):
     return mat[:, :-1], mat[:, -1]
 
 
-def apply_preprocessing(data):
-    """
-    Preprocess the data by normalizing and centering at 0.
-    :param data: numpy.ndarray, data.
-    :return: numpy.ndarray.
-    """
-    pass
-
-
 def get_tourney_reg_season(df):
     """
     Separate out the tournament and regular season data.
@@ -86,16 +76,3 @@ def get_tourney_reg_season(df):
     :return: (pandas.DataFrame, pandas.DataFrame)
     """
     return df[df.tourny == 0], df[df.tourny == 1]
-
-
-
-
-def predict_tournament_serial(df, predictor):
-    """
-    Predict the tournament in order from the beginning.
-    If we don't have
-    :param df: pandas.DataFrame, the tournament data.
-    :param predictor: sklearn.BaseEstimator.
-    :return: float, serial accuracy.
-    """
-    pass
